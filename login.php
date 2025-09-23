dab2d4221a7f50e5efb4b870ddd7ff8d720112e1
peereezz <inaki.perez@maristak.net>
2025-09-23 11:44:33 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+    </head>
+    <body>
+    <!--Hay que hacer un login sencillo de inicio de sesion -->
+        <form action="home.php" method="post">
+        <input type="text" id="usuario" name="usuario" placeholder="Usuario">
+        <input type="password" id="password" name="cont" placeholder="Password">
+        <button type="submit" id="btn_enviar" value="Inciar Sesion">Inciar Sesion</button>
+        </form>
+
+    </body>
+</html>
